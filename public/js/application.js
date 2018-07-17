072724aa745da3d2b40fff7ba2c41736e9784f71
Mukesh Patel <mkumar.dion@gmail.com>
2018-07-17 19:39:27 +0530

    Fixed options for geolocation

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,10 @@
 $(function() {
 	// generate unique user id
 	var droneId = Math.random().toString(16).substring(2,15);
-	var socket = io.connect('https://afb466da.ngrok.io');
+	var socket = io.connect('https://c161d06b.ngrok.io');
 	var infoList = $('#infoList');
 	var connects = {}, droneData  = [];
-
+	var option = { enableHighAccuracy: true, maximumAge: Infinity, timeout: Infinity };
 	socket.on('connect', function(){
 		socket.emit('send', {droneId: droneId});
 	})
@@ -16,7 +16,7 @@ $(function() {
 
 	// check whether browser supports geolocation api
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.getCurrentPosition(showPosition, positionError, option);
 	} else {
 		console.log('error.............');
 	}
@@ -36,7 +36,7 @@ $(function() {
 
 	//tracking function
 	function getLocation() {
-		navigator.geolocation.watchPosition(matchPosition);
+		navigator.geolocation.watchPosition(matchPosition, positionError, option);
 	}
 
 	//highlight drone when its not moving more than 10 sec
@@ -64,6 +64,15 @@ $(function() {
 		var data  = {droneId: droneId, latitude: lat, longitude: lng, speed: speed, timestamp: timestamp};
 		droneData.push(data);
 		socket.emit('send-data', data);
+	}
+
+	function positionError(error) {
+		var errors = {
+			1: 'Authorization fails', // permission denied
+			2: 'Can\'t detect your location', //position unavailable
+			3: 'Connection timeout' // timeout
+		};
+		showError('Error:' + errors[error.code]);
 	}
 
 	//match after 10 sec geolocation
