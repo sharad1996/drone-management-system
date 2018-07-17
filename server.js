ddf9d22987b9c7d8852f333d7345d2a18cf95396
Mukesh Patel <mkumar.dion@gmail.com>
2018-07-17 17:45:46 +0530

    Added package.json and intialize server.js file and add public folder

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var Static = require('node-static');
+var app = http.createServer(handler);
+var _ = require('lodash');
+var io = require('socket.io').listen(app);
+var port = 8080;
+
+var files = new Static.Server('./public');
+
+function handler(request, response) {
+  request.on('end', function () {
+    files.serve(request, response);
+  }).resume();
+}
+
+var droneData = [], central = '', updateData = [];
+
+//start socket programming
+io.on('connection', function (socket) {
+  //create central server
+  console.log(socket.id + " " + "connected");
+
+  //disconnect drone
+  socket.on('disconnect', function () {
+    updateData = droneData.filter(d => d.socketId !== socket.id);
+    console.log(socket.id + " " + "disconnected");
+  });
+});
+
+// start app on specified port
+app.listen(port);
+console.log('Your server goes on localhost:' + port);
